ce17f26b386a2aa2377dd0b84f66c12a3b394173
Rashaad Gardner <rashaadgardner@gmail.com>
2017-10-21 18:49:02 +0000

    Todo_Backend

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,7 +1,13 @@
 class TodoController < ApplicationController 
     def show
-        @todo_description="Make the Curriculum"
-        @todo_number=4
+        todo_id=params[:id]
+        if todo_id=="1"
+            @todo_description="Make the Curriculum"
+            @todo_number=4
+        elsif todo_id=="2"
+        @todo_description="Buy workshop supplies"
+        @todo_number=3
+        end
     end
     def index
     end
