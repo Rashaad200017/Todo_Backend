755564c6b6a8842b558e13d859785ef6a4a5ac18
Rashaad Gardner <rashaadgardner@gmail.com>
2017-10-22 16:33:05 +0000

    Add New Task

--- db/migrate/20171021195550_create_todos.rb
+++ db/migrate/20171021195550_create_todos.rb
@@ -2,7 +2,7 @@ class CreateTodos < ActiveRecord::Migration
   def change
     create_table :todos do |t|
       t.string :derscription
-      t.interger :pomodoro_estimate
+      t.integer :pomodoro_estimate
       t.boolean :complete
 
       t.timestamps null: false
